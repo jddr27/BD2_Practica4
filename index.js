fc76841a88c3c21e4b6dd42fa8ab52fe85a989f2
jddr27 <jddeleonr.inf2013@gmail.com>
2020-04-28 11:00:44 -0600

    jeje

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const HOST = '0.0.0.0';
 //”cassandra-driver” is in the node_modules folder. Redirect if necessary.
 var cassandra = require('cassandra-driver'); 
 //Replace Username and Password with your cluster settings
-var authProvider = new cassandra.auth.PlainTextAuthProvider('Username', 'Password');
+//var authProvider = new cassandra.auth.PlainTextAuthProvider('Username', 'Password');
 //Replace PublicIP with the IP addresses of your clusters
-var client = new cassandra.Client({contactPoints:['DC1N1'], authProvider: authProvider, keyspace:'practica2'});
+var client = new cassandra.Client({contactPoints:['DC1N1']/*, authProvider: authProvider, */keyspace:'practica2'});
  
 // App
 const app = express();
