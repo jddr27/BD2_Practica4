83a79f27b33eada10d1fc6643b08be84ac840ebe
jddr27 <jddeleonr.inf2013@gmail.com>
2020-04-28 12:27:04 -0600

    conectaaaaa

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 'use strict';
 
 const express = require('express');
-
-// Constants
-const PORT = 3001;
-const HOST = '0.0.0.0';
-
-//”cassandra-driver” is in the node_modules folder. Redirect if necessary.
-var cassandra = require('cassandra-driver'); 
-//Replace Username and Password with your cluster settings
-//var authProvider = new cassandra.auth.PlainTextAuthProvider('Username', 'Password');
-//Replace PublicIP with the IP addresses of your clusters
-var client = new cassandra.Client({contactPoints:['DC1N1'],/* authProvider: authProvider, */keyspace:'practica2'});
+const cassandra = require('cassandra-driver'); 
+const client = new cassandra.Client({contactPoints:['bd2_DC1N1_1','DC1N1'], keyspace:'practica2'});
+client.connect(function(err, result){
+    if(err){
+        console.log(err);
+    } else {
+        console.log('index: cassandra connected');
+    }
+    
+});
  
 // App
 const app = express();
 
 app.get('/', (req, res) => {
     const query = 'SELECT * FROM tickets WHERE fecha > \'2020-01-01\' AND fecha < \'2020-01-31\' ALLOW FILTERING;';
-    return client.execute(query, [ req.query.fruit_id ])
-        .then(result => {
-        return ('Fruta %s', result.rows[0].name)
-        }
-        );
+    client.execute(query,[], function(err, result){
+		if(err){
+			res.status(404).send({msg: err});
+		} else {
+			res.send('index %s', result.rows[0].titulo);
+		}
+	});
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(3001, function () {
+    console.log('Example app listening on port 3001!');
+  });
