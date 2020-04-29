0bc838b3f8dfa4b539846136b37eeb972f3a9d05
jddr27 <jddeleonr.inf2013@gmail.com>
2020-04-28 19:41:03 -0600

    siuh

--- index.js
+++ index.js
@@ -1,30 +1,82 @@
 'use strict';
-
 const express = require('express');
 const cassandra = require('cassandra-driver'); 
 const client = new cassandra.Client({contactPoints:['bd2_DC1N1_1:9042','bd2_DC1N2_1:9043','bd2_DC1N3_1:9044'], keyspace:'practica2'});
-client.connect(function(err, result){
+/*client.connect(function(err, result){
     if(err){
         console.log(err);
     } else {
         console.log('index: cassandra connected');
     }
     
-});
+});*/
  
-// App
-const app = express();
 
-app.get('/', (req, res) => {
-    const query = 'SELECT * FROM tickets WHERE fecha > \'2020-01-01\' AND fecha < \'2020-01-31\' ALLOW FILTERING;';
-    client.execute(query,[], function(err, result){
-		if(err){
-			res.status(404).send({msg: err});
-		} else {
-			res.send(result.rows);
-		}
-	});
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({ extended: true  }));
+app.use(bodyParser.json()); 
+app.use('/static', express.static(__dirname + '/public'));
+
+var salida = "";
+var arreglo = [];
+
+app.get('/',function(req, res){
+    res.render('main',{consola:salida, valores:arreglo});
 });
+
+app.post('/filtrar',function(req, res){
+    console.log('entro a filtrar');
+    salida = "";
+    if(req.body.fEmail == ""){
+        console.log("correo vacio");
+        var query = 'SELECT * FROM tickets WHERE fecha > \'?\' AND fecha < \'?\' ALLOW FILTERING;';
+        client.execute(query,[req.body.fIni, req.body.fFin], (err, result) => {
+            if(err){
+                salida = err;
+            } else {
+                arreglo = result.rows;
+            }
+        });
+    }
+    else{
+        console.log("correo no vacio");
+        var query = 'SELECT * FROM tickets WHERE fecha > \'?\' AND fecha < \'?\' AND email = \'?\' ALLOW FILTERING;';
+        client.execute(query,[req.body.fIni, req.body.fFin, req.body.fEmail], (err, result) => {
+            if(err){
+                salida = err;
+            } else {
+                arreglo = result.rows;
+            }
+        });
+    }
+    
+    res.redirect('/');
+});
+
+
+app.post('/crear',function(req, res){
+    arreglo = [];
+    console.log('entro a crear');
+    console.log(req.body);
+    var id = Math.floor(Math.random() * (100000000 - 10000000)) + 10000000;
+    var id2 = Math.floor(Math.random() * (10 - 0));
+    var idfinal = id.toString() + '-' + id2.toString();
+    console.log("idfinal: " + idfinal);
+
+    const query = 'INSERT INTO tickets (idTicket, titulo, descripcion, email, fecha) VALUES (?, ?, ?, ?, ?)';
+    client.execute(query,[idfinal, req.body.nTitulo, req.body.nDescri, req.body.nEmail, req.body.nFecha], function(err, result){
+		if(err){
+			salida = err;
+		} else {
+            salida = result.rows;
+		}
+    });
+    res.redirect('/');
+});
+  
 
 app.listen(3001, function () {
     console.log('Example app listening on port 3001!');
